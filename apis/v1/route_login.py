85bb993d55ac8d50340973657c9df544fc2b6f7b
ELITE <155517447+EliteKnightDeveloper@users.noreply.github.com>
2024-02-14 07:47:52 +0900

    login function

--- apis/v1/route_login.py
+++ apis/v1/route_login.py
@@ -28,7 +28,7 @@ def authenticate_user(email: str, password: str, db: Session):
     return user
 
 
-@router.post("/token", response_model=Token)
+@router.post("/login", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
